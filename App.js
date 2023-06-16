5e5250f1aa9bb5ea6ac1f33b753fa08b8f9b9400
Juan Quiroz <juandavidlopezquiroz@gmail.com>
2023-06-16 10:52:13 -0500

    parcial

---
+++ App.js
@@ -0,0 +1,7 @@
+let estudiantes = [
+    { nombre: "Juan", edad: 18, carrera: "Ingeniería", promedio: 4.0, activo: true },
+    { nombre: "Ana", edad: 16, carrera: "Ingenieria", promedio: 4.5, activo: false },
+    { nombre: "Farid", edad: 16, carrera: "Ingenieria", promedio: 3.2, activo: true },
+    { nombre: "juan manuel", edad: 17, carrera: "Ingenieria", promedio: 3.5, activo: false }
+];
+console.log(estudiantes);
